26ddfab97e9b9b1ae6ec09b923b0a54a2c2ba59a
Amaury Garcia <amaugarci@outlook.com>
2022-02-22 14:27:12 +0530

    Update font

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -30,7 +30,7 @@ class _SoloLuxuryAppState extends State<SoloLuxuryApp> {
         theme: ThemeData(
           backgroundColor: primaryWhite,
           scaffoldBackgroundColor: primaryWhite,
-          fontFamily: 'Montserrat',
+          fontFamily: 'Poppins',
           hintColor: regularGrey,
           iconTheme: const IconThemeData(
             color: regularGrey,
