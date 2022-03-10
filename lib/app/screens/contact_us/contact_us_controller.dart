80b34e603207f51b9fa35e69f873393036304594
Amaury Garcia <amaugarci@outlook.com>
2022-03-10 12:49:40 +0530

    Few Change

--- lib/app/screens/contact_us/contact_us_controller.dart
+++ lib/app/screens/contact_us/contact_us_controller.dart
@@ -3,9 +3,8 @@ import 'package:get/get.dart';
 
 class ContactUsController extends GetxController {
   Rx<GlobalKey<ScaffoldState>> scaffoldKey = GlobalKey<ScaffoldState>().obs;
-
   RxInt index = 0.obs;
-
+  
   @override
   void onInit() {
     super.onInit();
