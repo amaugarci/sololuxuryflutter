4c36474957d41fd184cbcd22a6af06bb43f08f58
Amaury Garcia <amaugarci@outlook.com>
2022-03-15 10:22:59 +0530

    serach Ui changes COmpleted TAsk ID= 15570 Date=15/3

--- lib/app/screens/search/search_screen.dart
+++ lib/app/screens/search/search_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 import 'package:solo_luxury/utils/lang_directory/language_constant.dart';
-
 import '../../../utils/image_constant.dart';
 import '../../utils/app_asset.dart';
 import '../../utils/colors.dart';
@@ -13,229 +12,152 @@ class SearchScreen extends GetView<SearchController> {
 
   @override
   Widget build(BuildContext context) {
-    return Obx(() => Scaffold(
-          key: controller.scaffoldKey.value,
+    return Obx(
+      () => Scaffold(
+        key: controller.scaffoldKey.value,
+        backgroundColor: backGroundColor,
+        appBar: AppBar(
           backgroundColor: backGroundColor,
-          appBar: AppBar(
-            backgroundColor: backGroundColor,
-            elevation: 0,
-            iconTheme: const IconThemeData(color: Colors.black),
-            centerTitle: true,
-            leading: InkWell(
-              onTap: () {
-                controller.scaffoldKey.value.currentState!.openDrawer();
-              },
+          elevation: 0,
+          iconTheme: const IconThemeData(color: Colors.black),
+          centerTitle: true,
+          leading: InkWell(
+            onTap: () {
+              controller.scaffoldKey.value.currentState!.openDrawer();
+            },
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: SvgPicture.asset(ImageConstant.menuIcon),
+            ),
+          ),
+          actions: [
+            InkWell(
+              onTap: () {},
               child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: SvgPicture.asset(ImageConstant.menuIcon),
+                padding:
+                    const EdgeInsets.symmetric(vertical: 16.0, horizontal: 8.0),
+                child: SvgPicture.asset(ImageConstant.searchIcon),
               ),
             ),
-            actions: [
-              InkWell(
-                onTap: () {},
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                      vertical: 16.0, horizontal: 8.0),
-                  child: SvgPicture.asset(ImageConstant.searchIcon),
-                ),
+            InkWell(
+              onTap: () {},
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 16.0, horizontal: 8.0),
+                child: SvgPicture.asset(ImageConstant.heartIcon),
               ),
-              InkWell(
-                onTap: () {},
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                      vertical: 16.0, horizontal: 8.0),
-                  child: SvgPicture.asset(ImageConstant.heartIcon),
-                ),
+            ),
+            InkWell(
+              onTap: () {},
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 16.0, horizontal: 8.0),
+                child: SvgPicture.asset(ImageConstant.shoppingCartIcon,
+                    color: Colors.black),
               ),
-              InkWell(
-                onTap: () {},
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                      vertical: 16.0, horizontal: 8.0),
-                  child: SvgPicture.asset(ImageConstant.shoppingCartIcon,
-                      color: Colors.black),
-                ),
-              ),
-            ],
-            title: Image.asset(AppAsset.logo, width: 110),
-            /*bottom: PreferredSize(
-          preferredSize: Size(Get.width, 60),
-          child: const HeaderWidget(),
-        ),*/
-          ),
-          body: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 5),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                //  filterWidget(),
-                const SizedBox(height: 15),
-                filterDropDown(),
-                const SizedBox(height: 10),
-                Flexible(
-                  child: DefaultTabController(
-                      length: 3, // length of tabs
-                      initialIndex: 0,
-                      child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.stretch,
-                          children: <Widget>[
-                            Container(
-                              child: TabBar(
-                                labelColor: selectedTabColor,
-                                unselectedLabelColor: unselectedTabColor,
-                                tabs: [
-                                  Tab(
-                                    text: LanguageConstant.menText.tr,
-                                  ),
-                                  Tab(text: LanguageConstant.womenText.tr),
-                                  Tab(text: LanguageConstant.kidsText.tr),
-                                ],
+            ),
+          ],
+          title: Image.asset(AppAsset.logo, width: 110),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 5),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              //  filterWidget(),
+              const SizedBox(height: 15),
+              filterDropDown(),
+              const SizedBox(height: 10),
+              Flexible(
+                child: DefaultTabController(
+                  length: 3, // length of tabs
+                  initialIndex: 0,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 40),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: <Widget>[
+                        Container(
+                          child: TabBar(
+                            indicatorColor: buttoncolor,
+                            indicatorSize: TabBarIndicatorSize.tab,
+                            labelColor: selectedTabColor,
+                            unselectedLabelColor: unselectedTabColor,
+                            tabs: [
+                              Tab(
+                                text: LanguageConstant.menText.tr,
+                              ),
+                              Tab(text: LanguageConstant.womenText.tr),
+                              Tab(text: LanguageConstant.kidsText.tr),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          height: 400, //height of TabBarView
+                          decoration: const BoxDecoration(
+                            border: Border(
+                              top: BorderSide(
+                                color: Colors.grey,
+                                width: 0.5,
                               ),
                             ),
-                            Container(
-                                height: 400, //height of TabBarView
-                                decoration: const BoxDecoration(
-                                    border: Border(
-                                        top: BorderSide(
-                                            color: Colors.grey, width: 0.5))),
-                                child: TabBarView(children: <Widget>[
-                                  Container(
-                                    child: Column(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.start,
-                                      children: [
-                                        Padding(
-                                          padding: const EdgeInsets.only(
-                                              left: 10.0, top: 10),
-                                          child: Row(
-                                            mainAxisAlignment:
-                                                MainAxisAlignment.start,
-                                            children: [
-                                              const Text('Clothing',
-                                                  style: TextStyle(
-                                                      fontSize: 14,
-                                                      fontWeight:
-                                                          FontWeight.w400)),
-                                              SizedBox(
-                                                width: Get.width * 0.7,
-                                              ),
-                                              const Icon(
-                                                Icons.arrow_forward_ios,
-                                                color: selectedTabColor,
-                                                size: 12,
-                                              ),
-                                            ],
-                                          ),
-                                        ),
-                                        const SizedBox(
-                                          height: 10,
-                                        ),
-                                        Padding(
-                                          padding: const EdgeInsets.only(
-                                              left: 10.0, top: 10),
-                                          child: Row(
-                                            mainAxisAlignment:
-                                                MainAxisAlignment.start,
-                                            children: [
-                                              const Text('Bags',
-                                                  style: TextStyle(
-                                                      fontSize: 14,
-                                                      fontWeight:
-                                                          FontWeight.w400)),
-                                              SizedBox(
-                                                width: Get.width * 0.75,
-                                              ),
-                                              const Icon(
-                                                Icons.arrow_forward_ios,
-                                                color: selectedTabColor,
-                                                size: 12,
-                                              ),
-                                            ],
-                                          ),
-                                        ),
-                                        const SizedBox(
-                                          height: 10,
-                                        ),
-                                        Padding(
-                                          padding: const EdgeInsets.only(
-                                              left: 10.0, top: 10),
-                                          child: Row(
-                                            mainAxisAlignment:
-                                                MainAxisAlignment.start,
-                                            children: [
-                                              const Text('Footwear',
-                                                  style: TextStyle(
-                                                      fontSize: 14,
-                                                      fontWeight:
-                                                          FontWeight.w400)),
-                                              SizedBox(
-                                                width: Get.width * 0.68,
-                                              ),
-                                              const Icon(
-                                                Icons.arrow_forward_ios,
-                                                color: selectedTabColor,
-                                                size: 12,
-                                              ),
-                                            ],
-                                          ),
-                                        ),
-                                      ],
-                                    ),
+                          ),
+                          child: TabBarView(
+                            children: <Widget>[
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  categoriesOptions(text: "Clothing"),
+                                  const SizedBox(
+                                    height: 10,
                                   ),
-                                  Container(
-                                    child: Column(
-                                      children: [
-                                        Text('Display Tab 2',
-                                            style: TextStyle(
-                                                fontSize: 22,
-                                                fontWeight: FontWeight.bold)),
-                                      ],
-                                    ),
+                                  categoriesOptions(text: "Bags"),
+                                  const SizedBox(
+                                    height: 10,
                                   ),
-                                  const Center(
-                                    child: Text('Display Tab 3',
-                                        style: TextStyle(
-                                            fontSize: 22,
-                                            fontWeight: FontWeight.bold)),
+                                  categoriesOptions(text: "Footwear"),
+                                  const SizedBox(
+                                    height: 10,
                                   ),
-                                ]))
-                          ])),
+                                  categoriesOptions(text: "Accessories"),
+                                  const SizedBox(
+                                    height: 10,
+                                  ),
+                                  categoriesOptions(text: "Intimates"),
+                                ],
+                              ),
+                              Column(
+                                children: const [
+                                  Text('Display Tab 2',
+                                      style: TextStyle(
+                                          fontSize: 22,
+                                          fontWeight: FontWeight.bold)),
+                                ],
+                              ),
+                              const Center(
+                                child: Text(
+                                  'Display Tab 3',
+                                  style: TextStyle(
+                                    fontSize: 22,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
-                // FutureBuilder<List<ProductModel>>(
-                //   future: controller.getHomeProducts("12"),
-                //   builder: (context, snapshot) {
-                //     if (snapshot.hasData) {
-                //       return Expanded(
-                //         child: GridView.builder(
-                //           padding: EdgeInsets.zero,
-                //           shrinkWrap: true,
-                //           gridDelegate:
-                //               const SliverGridDelegateWithFixedCrossAxisCount(
-                //             crossAxisCount: 2,
-                //             mainAxisSpacing: 10,
-                //             crossAxisSpacing: 12.5,
-                //             childAspectRatio: 0.55,
-                //           ),
-                //           itemBuilder: (_, index) {
-                //             return Product(
-                //                 product:
-                //                     controller.homeCategoryProductList[index]);
-                //           },
-                //           itemCount:
-                //               controller.homeCategoryProductList.value.length,
-                //         ),
-                //       );
-                //     } else {
-                //       return Container();
-                //     }
-                //   },
-                // ),
+              ),
 
-                const SizedBox(height: 20),
-              ],
-            ),
+              const SizedBox(height: 20),
+            ],
           ),
-        ));
+        ),
+      ),
+    );
   }
 
   Widget filterWidget() {
@@ -278,26 +200,45 @@ class SearchScreen extends GetView<SearchController> {
     );
   }
 
+  categoriesOptions({var text}) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            text,
+            style: const TextStyle(fontSize: 14, fontWeight: FontWeight.w400),
+          ),
+          const Icon(
+            Icons.arrow_forward_ios,
+            color: selectedTabColor,
+            size: 12,
+          ),
+        ],
+      ),
+    );
+  }
+
   Widget filterDropDown() {
     return Container(
       height: 50,
       width: Get.width * 0.85,
-      padding: const EdgeInsets.symmetric(horizontal: 10),
+      padding: const EdgeInsets.symmetric(horizontal: 5),
       decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(4.0),
         border: Border.all(color: appColor, width: 1.4),
       ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          const Icon(
+      child: TextFormField(
+        decoration: InputDecoration(
+          prefixIcon: const Icon(
             Icons.search,
             color: Colors.black54,
           ),
-          Text(
-            LanguageConstant.searchText.tr,
-            style: const TextStyle(color: Colors.black54),
-          ),
-        ],
+          border: InputBorder.none,
+          hintText: LanguageConstant.searchText.tr,
+          hintStyle: const TextStyle(color: Colors.black54),
+        ),
       ),
     );
   }
